5f30a14e49504995d19917a979694449b194a78a
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2025-06-19 16:41:09 -0300

    exclusão weatherForecast

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = new List<string> { "User1", "User2", "User3" };
+
+            return Ok(users);
+        }
+    }
+}
