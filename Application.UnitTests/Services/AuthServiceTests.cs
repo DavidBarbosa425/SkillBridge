31a9292ef4b6f6c9faf71b810b942f14f877ec9d
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2025-07-08 14:36:00 -0300

    test: adiciona teste unitário para falha no AuthService.RegisterUserAsync

--- Application.UnitTests/Services/AuthServiceTests.cs
+++ Application.UnitTests/Services/AuthServiceTests.cs
@@ -87,5 +87,53 @@ namespace Application.UnitTests.Services
             Assert.True(result.Success);
             Assert.Equal("Usuário criado com sucesso. Um E-mail de Confirmação foi enviado para sua caixa de entrada.", result.Message);
         }
+
+        [Fact]
+        public async Task RegisterUserAsync_ShouldReturnError_WhenUserIsNotCreated()
+        {
+            // Arrange
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var applicationMapperMock = new Mock<IApplicationMapper>();
+            var validatorServiceMock = new Mock<IValidatorService>();
+            var emailServiceMock = new Mock<IEmailService>();
+            var emailConfirmationServiceMock = new Mock<IEmailConfirmationService>();
+
+            userRepositoryMock
+                .Setup(x => x.AddAsync(It.IsAny<User>()))
+                .ReturnsAsync(Result<string>.Failure("Erro ao criar Usuário, tente novamente mais tarde"));
+
+            var user = new User
+            {
+                Name = "Teste",
+                Email = "teste@teste.com",
+                Password = "123456"
+            };
+
+            applicationMapperMock
+                .Setup(x => x.User.ToUser(It.IsAny<RegisterUserDto>()))
+                .Returns(user);
+
+            var authService = new AuthService(
+                userRepositoryMock.Object,
+                applicationMapperMock.Object,
+                validatorServiceMock.Object,
+                emailServiceMock.Object,
+                emailConfirmationServiceMock.Object
+            );
+
+            var dto = new RegisterUserDto
+            {
+                Name = "Teste",
+                Email = "teste@teste.com",
+                Password = "123456"
+            };
+
+            // Act
+            var result = await authService.RegisterUserAsync(dto);
+
+            // Assert
+            Assert.False(result.Success);
+            Assert.Equal("Erro ao criar Usuário, tente novamente mais tarde", result.Message);
+        }
     }
 }
