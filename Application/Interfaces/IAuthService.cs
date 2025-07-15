adea79d6d92ef8af998954d466872cabfee3dd8a
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2025-07-15 15:45:46 -0300

    refactor: authservice application

--- Application/Interfaces/IAuthService.cs
+++ Application/Interfaces/IAuthService.cs
@@ -1,12 +1,14 @@
 ﻿using Application.DTOs;
 using Domain.Common;
+using Domain.Entities;
 
 namespace Application.Interfaces
 {
     public interface IAuthService
     {
-        Task LoginAsync(LoginDto dto);
         Task<Result> RegisterUserAsync(RegisterUserDto dto);
+        Task<Result<SendEmail>> GenerateEmailConfirmation(UserDto userDto);
         Task<Result> ConfirmationUserEmailAsync(Guid userId, string token);
+        Task LoginAsync(LoginDto dto);
     }
 }
