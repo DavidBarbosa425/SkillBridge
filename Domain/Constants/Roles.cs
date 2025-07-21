996a842716f7b9e4aa095e2f0b2b7d7e814c05f2
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2025-07-21 15:11:13 -0300

    feature: adiciona constant roles

---
+++ Domain/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Constants
+{
+    public static class Roles
+    {
+        public const string Developer = "Developer";
+        public const string Company = "Company";
+        public const string Admin = "Admin";
+    }
+}
