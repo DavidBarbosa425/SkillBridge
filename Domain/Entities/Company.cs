ced44de17c339c7917b90119d5928728f84aee4b
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2025-08-05 13:26:45 -0300

    refactor: migration domainUser

---
+++ Domain/Entities/Company.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities
+{
+    public class Company
+    {
+        public string Name { get; set; } = string.Empty;
+        public string CNPJ { get; set; } = string.Empty;
+    }
+}
