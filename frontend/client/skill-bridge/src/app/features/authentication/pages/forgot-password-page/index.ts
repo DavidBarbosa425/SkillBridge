3b74112b9a17378849ddd4f02fcad5820f31d319
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2025-08-29 17:28:15 -0300

    feat: adiciona index.ts forgot-password

--- frontend/client/skill-bridge/src/app/features/authentication/pages/forgot-password-page/index.ts
+++ frontend/client/skill-bridge/src/app/features/authentication/pages/forgot-password-page/index.ts
@@ -1,1 +1,1 @@
-
+export * from './forgot-password-page';
--- frontend/client/skill-bridge/src/app/features/authentication/pages/index.ts
+++ frontend/client/skill-bridge/src/app/features/authentication/pages/index.ts
@@ -1,2 +1,4 @@
 export * from './confirm-email-page';
 export * from './login-page';
+export * from './forgot-password-page';
+export * from './reset-password-page';
